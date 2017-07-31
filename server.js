198757341f8c0bdd1565650227e393218b6e0cbb
Pujasvi <pujasvirakheja1@gmail.com>
2017-07-31 14:38:14 +0530

    some text

--- server.js
+++ server.js
@@ -41,10 +41,10 @@ var transporter=nodemailer.createTransport({
     }
 });
 
+app.listen(process.env.PORT || 5000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
-app.listen(4100,function(){
-    console.log("server running on port 4100");
-})
 
 
 
