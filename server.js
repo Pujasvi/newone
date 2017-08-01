83e680925ec08667e2db973906a9388f670ca052
Pujasvi <pujasvirakheja1@gmail.com>
2017-08-01 13:16:53 +0530

    some chn

--- server.js
+++ server.js
@@ -196,9 +196,9 @@ var id_my;
 
 app.get('/verify',function(req,res){
     console.log(req.protocol+":/"+req.get('host'));
-    if((req.protocol+"://"+req.get('host'))==("http://"+host1))
-    {
-        console.log("Domain is matched. Information is from Authentic email");
+    //if((req.protocol+"://"+req.get('host'))==("http://"+host1))
+    //{
+      //  console.log("Domain is matched. Information is from Authentic email");
         if(req.query.id==rand)
         {
             console.log("email is verified");
@@ -213,11 +213,11 @@ app.get('/verify',function(req,res){
             console.log("email is not verified");
             res.end("<h1>Bad Request</h1>");
         }
-    }
-    else
+    //}
+    /*else
     {
         res.end("<h1>Request is from unknown source");
-    }
+    }*/
 });
 
 
